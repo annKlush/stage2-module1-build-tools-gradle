2a3379c62a0e54482a5d08e304b5c62e5d1b4d41
annKlush <109134951+annKlush@users.noreply.github.com>
2023-09-27 15:03:04 +0300

    com

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -4,6 +4,25 @@ import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        //magic happens here
+        for (String str : args) {
+            if (!isPositiveNumber(str)) {
+                return false; // If any string is not a positive number, return false
+            }
+        }
+        return true; // All strings in the list are positive numbers
     }
+
+    public static boolean isPositiveNumber(String str) {
+        if (str == null || str.isEmpty()) {
+            return false; // Empty or null strings are not positive numbers
+        }
+
+        try {
+            double number = Double.parseDouble(str); // Attempt to parse the string as a double
+            return number > 0; // Check if the parsed number is greater than zero
+        } catch (NumberFormatException e) {
+            return false; // Parsing failed, so it's not a valid number
+        }
+    }
+
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,15 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-         //here magic will happen
+        if (str == null || str.isEmpty()) {
+            return false; // Empty or null strings are not positive numbers
+        }
+
+        try {
+            double number = Double.parseDouble(str); // Attempt to parse the string as a double
+            return number > 0; // Check if the parsed number is greater than zero
+        } catch (NumberFormatException e) {
+            return false; // Parsing failed, so it's not a valid number
+        }
     }
 }
